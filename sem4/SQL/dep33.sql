7b9da5f2083ad20166b0a8e0926cc81b00d5b913
Sambhav Jain <sambhavjain2612@gmail.com>
2018-01-24 13:42:39 +0530

    update sql

--- sem4/SQL/dep33.sql
+++ sem4/SQL/dep33.sql
@@ -4,3 +4,10 @@ Dname varchar(50),
 location varchar(50) Default 'New Delhi', 
 primary key(Dno)
 );
+
+DNO	DNAME		LOCATION
+10	Accounting	New York
+30	Sales		Chicago
+20	Research	Dallas
+50	Marketing	New Delhi
+40	Operation	Boston
