c868eeaafea445d216efcf2b3165953403c98bc0
Sambhav Jain <sambhavjain2612@gmail.com>
2018-05-03 09:33:59 +0530

    Update 1.php

--- sem4/PHP/1.php
+++ sem4/PHP/1.php
@@ -1,16 +1,17 @@
 <?php
 
-$a=10;
-$b=12;
-$c=15;
+$a=10; 
+$b=50;
+$c=3;
 
-if($a>=$b){
-  if($a>=$c){ echo $a; }
-  else { echo $c; }
-}
-else{
-  if($b>=$c){ echo $b; }
-  else { echo $c; }
-}
+if ($a>=$b) {
+    if ($a>=$c) {
+        echo $a;
+    } else {
+        echo $c;
+    }
+} else {
+    echo $b;
+} 
 
 ?>
